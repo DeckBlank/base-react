fc2196be660d14def1280b44c3b5e2212aed7082
deckblank@gmail.com <Jose Luciano Guzman Santiago>
2022-04-27 10:40:49 -0500

    explain ui kit

--- src/Views/Dashboard/Dashboard.js
+++ src/Views/Dashboard/Dashboard.js
@@ -1,96 +1,94 @@
-import React from 'react';
-import Table from '../../components/Table/Table';
-const handleSubmit = async e => {
+import React from "react";
+import Table from "../../components/Table/Table";
+const handleSubmit = async (e) => {
   e.preventDefault();
 
-  console.log('eliminar token');
-
-}
+  console.log("eliminar token");
+};
 
 export default function Dashboard() {
-
   const columns = React.useMemo(
     () => [
       {
-        Header: 'Name',
+        Header: "Name",
         columns: [
           {
-            Header: 'First Name',
-            accessor: 'firstName',
+            Header: "First Name",
+            accessor: "firstName",
           },
           {
-            Header: 'Last Name',
-            accessor: 'lastName',
+            Header: "Last Name",
+            accessor: "lastName",
           },
         ],
       },
       {
-        Header: 'Info',
+        Header: "Info",
         columns: [
           {
-            Header: 'Age',
-            accessor: 'age',
+            Header: "Age",
+            accessor: "age",
           },
           {
-            Header: 'Visits',
-            accessor: 'visits',
+            Header: "Visits",
+            accessor: "visits",
           },
           {
-            Header: 'Status',
-            accessor: 'status',
+            Header: "Status",
+            accessor: "status",
           },
           {
-            Header: 'Profile Progress',
-            accessor: 'progress',
+            Header: "Profile Progress",
+            accessor: "progress",
           },
         ],
       },
     ],
     []
-  )
-  
-  const [data, setData] = React.useState([])
-  const [loading, setLoading] = React.useState(false)
-  const [pageCount, setPageCount] = React.useState(0)
-  const fetchIdRef = React.useRef(0)
-  
+  );
+
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [pageCount, setPageCount] = React.useState(0);
+  const fetchIdRef = React.useRef(0);
+
   const fetchData = React.useCallback(({ pageSize, pageIndex }) => {
-  
-    const fetchId = ++fetchIdRef.current
-  
-    setLoading(true)
-  
+    const fetchId = ++fetchIdRef.current;
+
+    setLoading(true);
+
     setTimeout(() => {
       if (fetchId === fetchIdRef.current) {
-        const startRow = pageSize * pageIndex
-        const endRow = startRow + pageSize
-          let data = [];
-          for (let i = startRow; i < endRow; i++) {
-              data.push({
-                  firstName: 'Ricky' + i,
-                  lastName: 'Martin' + i,
-                  age: Math.floor(Math.random() * 60),
-                  visits: Math.floor(Math.random() * 100),
-                  progress: Math.floor(Math.random() * 100),
-                  status:  'single'
-              });
-          }
-        setData(data)
-        setPageCount(Math.ceil(1000 / pageSize))
-  
-        setLoading(false)
+        const startRow = pageSize * pageIndex;
+        const endRow = startRow + pageSize;
+        let data = [];
+        for (let i = startRow; i < endRow; i++) {
+          data.push({
+            firstName: "Ricky" + i,
+            lastName: "Martin" + i,
+            age: Math.floor(Math.random() * 60),
+            visits: Math.floor(Math.random() * 100),
+            progress: Math.floor(Math.random() * 100),
+            status: "single",
+          });
+        }
+        setData(data);
+        setPageCount(Math.ceil(1000 / pageSize));
+
+        setLoading(false);
       }
-    }, 1000)
-  }, [])
-  
-  return(
+    }, 1000);
+  }, []);
+
+  return (
     <div className="login-wrapper">
       <h2>Dashboard</h2>
       <form onSubmit={handleSubmit}>
-          <div>
-            <button type="submit">Signout</button>
-          </div>
+        <div>
+          <button type="submit">Signout</button>
+        </div>
       </form>
+      <br />
       <Table
         columns={columns}
         data={data}
--- src/Views/Preferences/Preferences.js
+++ src/Views/Preferences/Preferences.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React,{useRef} from 'react';
+import Button from '../../uiKit/Button/Button';
 
 export default function Preferences() {
+
+  let color = useRef('secondary');
+  const saludar = () => {
+    alert('hola')
+  }
+  const chau = () => {
+    alert('chau')
+  }
   return(
-    <h2>Preferences</h2>
+    <div>
+      <h2>Preferences</h2>
+      <Button 
+        text={"Agendar clase"}
+        onClick={saludar}
+      />
+      <Button 
+        color={color.current} 
+        className={'btn-block'}
+        text={"despedirse"}
+        onClick={chau}
+      />
+      {/* <Card 
+        img={color.current} 
+        text={"despedirse"}
+        onClick={accion}
+      /> */}
+    </div>
   );
 }
