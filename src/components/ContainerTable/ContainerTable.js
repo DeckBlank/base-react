fb92949e06f57fd33dc683fd6bfbbbf1ad909db6
Rosamaria Rodriguez <rosamaria.rodriguez@mablovers.com>
2022-06-07 13:55:33 -0500

    STYLE: Refactor code of table

--- src/components/ContainerTable/ContainerTable.js
+++ src/components/ContainerTable/ContainerTable.js
@@ -34,23 +34,25 @@ const renderBody = (item, index) => (
 const CustomersList = () => {
   return (
     <div>
-      <h2 className="page-header">Contenido</h2>
-      <Button className="btn btn-radius txt-white btn-fuchsia btn-hover-fuchsia btn-flex-row">
+      <h2 className="page-header">Inicio</h2>
+      <div className="section-table">
+      <Button className="btn btn-width btn-radius txt-white btn-fuchsia btn-hover-fuchsia btn-flex-row">
         <i className="fas fa-solid fa-bell"></i>
         Filtro
       </Button>
-      <SearchBtn />
-      <Table
-        limit="10"
-        headData={customerTableHead}
-        renderHead={(item, index) => renderHead(item, index)}
-        bodyData={customerList}
-        renderBody={(item, index) => renderBody(item, index)}
-      />
-      <Button className="btn btn-radius txt-white btn-green btn-hover-green btn-flex-row">
-        <i className="fas fa-solid fa-bell"></i>
-        Excel
-      </Button>
+        <SearchBtn />
+        <Table
+          limit="10"
+          headData={customerTableHead}
+          renderHead={(item, index) => renderHead(item, index)}
+          bodyData={customerList}
+          renderBody={(item, index) => renderBody(item, index)}
+        />
+        <Button className="btn btn-width btn-radius txt-white btn-green btn-hover-green btn-flex-row">
+          <i className="fas fa-solid fa-bell"></i>
+          Excel
+        </Button>
+      </div>
     </div>
   );
 };
