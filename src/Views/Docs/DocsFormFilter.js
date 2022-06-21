42ab85a48c34e390f0764d390a36dcda9063edc2
Rosamaria Rodriguez <rosamaria.rodriguez@mablovers.com>
2022-06-21 10:28:44 -0500

    REFACTOR: Style componente form filter example

--- src/Views/Docs/DocsFormFilter.js
+++ src/Views/Docs/DocsFormFilter.js
@@ -1,52 +1,52 @@
-import React from 'react'
-import FormFilter from '../../uiKit/FormFilter/FormFilter'
-import FormFilterEjm from '../../uiKit/FormFilter/FormFilterEjm'
-import RadioBtn from '../../uiKit/RadioBtn/RadioBtn'
-import SwitchBtn from '../../uiKit/SwitchBtn/SwitchBtn'
+import React from "react";
+import FormFilter from "../../uiKit/FormFilter/FormFilter";
+import FormFilterEjm from "../../uiKit/FormFilter/FormFilterEjm";
+import RadioBtn from "../../uiKit/RadioBtn/RadioBtn";
+import SwitchBtn from "../../uiKit/SwitchBtn/SwitchBtn";
 
 const DocsFormFilter = () => {
   return (
     <div>
-      <div>
-        <h2>RADIO BUTTONS</h2>
-        <br />
-        <RadioBtn text="Radio Button" type="radio"/>
-        <br />
-
-        <h2>SWITCH BUTTONS</h2>
-        <br />
-        <SwitchBtn text="Switch Button" type="radio"/>
-        <br />
-        <br />
-        
+      <div className="grid-responsive-docs-filtercard">
+        <div>
+          <h2>RADIO BUTTONS</h2>
+          <br />
+          <RadioBtn text="Radio Button" type="radio" />
+        </div>
+        <div>
+          <h2>SWITCH BUTTONS</h2>
+          <br />
+          <SwitchBtn text="Switch Button" type="radio" />
+        </div>
       </div>
-      <h3>FORM FILTERS RADIO BTN</h3>
-      <br />      
-      <div>
-        <FormFilter>
-          <RadioBtn text="Radio Button" type="radio"/>
-          <RadioBtn text="Radio Button" type="radio"/>
-        </FormFilter>        
+      <br />
+      <div className="grid-responsive-docs-filtercard">
+        <div>
+          <h2>FORM FILTERS RADIO BTN</h2>
+          <br />
+          <FormFilter>
+            <RadioBtn text="Radio Button" type="radio" />
+            <RadioBtn text="Radio Button" type="radio" />
+          </FormFilter>
+        </div>
+        <div>
+          <h2>FORM FILTERS RADIO BTN</h2>
+          <br />
+          <FormFilter>
+            <SwitchBtn text="Switch Button" />
+            <SwitchBtn text="Switch Button" />
+          </FormFilter>
+        </div>
       </div>
       <br />
 
-      <h3>FORM FILTERS RADIO BTN</h3>
-      <br />      
-      <div>
-        <FormFilter>
-          <SwitchBtn text="Switch Button"/>
-          <SwitchBtn text="Switch Button"/>
-        </FormFilter>        
-      </div>
-      <br />
-
-      <h3>FORM FILTERS FUNCTIONAL</h3>
+      <h2>FORM FILTERS FUNCTIONAL</h2>
       <br />
       <div>
-        <FormFilterEjm/>
+        <FormFilterEjm />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DocsFormFilter
+export default DocsFormFilter;
