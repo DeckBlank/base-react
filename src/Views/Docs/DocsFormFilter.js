2c1cb2239bcd80495b52d2f6cd38d30fa8d5a164
Rosamaria Rodriguez <rosamaria.rodriguez@mablovers.com>
2022-06-20 19:10:03 -0500

    CREATE: Switch btn components

--- src/Views/Docs/DocsFormFilter.js
+++ src/Views/Docs/DocsFormFilter.js
@@ -2,6 +2,7 @@ import React from 'react'
 import FormFilter from '../../uiKit/FormFilter/FormFilter'
 import FormFilterEjm from '../../uiKit/FormFilter/FormFilterEjm'
 import RadioBtn from '../../uiKit/RadioBtn/RadioBtn'
+import SwitchBtn from '../../uiKit/SwitchBtn/SwitchBtn'
 
 const DocsFormFilter = () => {
   return (
@@ -10,12 +11,19 @@ const DocsFormFilter = () => {
         <h2>RADIO BUTTONS</h2>
         <br />
         <RadioBtn text="Radio Button" type="radio"/>
+        <br />
+
+        <h2>SWITCH BUTTONS</h2>
+        <br />
+        <SwitchBtn text="Switch Button" type="radio"/>
+        <br />
+        
       </div>
       <h2>FORM FILTERS</h2>
       <br />      
       <div>
         <FormFilter>
-          <RadioBtn text="Radio Button" type="radio" checked/>
+          <RadioBtn text="Radio Button" type="radio"/>
           <RadioBtn text="Radio Button" type="radio"/>
         </FormFilter>        
       </div>
---
+++ src/uiKit/SwitchBtn/SwitchBtn.js
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./_SwitchBtn.scss";
+
+const SwitchBtn = (props) => {
+  let { id, name, text, value } = props;
+
+  return (
+    <label id={id} class="switch">
+      <input
+        type="checkbox"
+        name={name}
+        value={value}
+      />
+      <span class="slider round"></span>
+      {text}
+    </label>
+  );
+};
+
+SwitchBtn.propTypes = {
+  id: PropTypes.string,
+  type: PropTypes.string,
+  text: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.string,
+};
+
+export default SwitchBtn;
+
+// value="Male"
+// checked={this.state.selectedOption === "Male"}
+// onChange={this.onValueChange}
+
+// import React from "react";
+// import './_RadioBtn.scss'
+
+// const RadioBtn = () => {
+//   return (
+//     <form className="radiobtn__form" action="">
+//       <label class="form-control">
+//         <input type="radio" name="radio" />
+//         Radio
+//       </label>
+
+//       <label class="form-control">
+//         <input type="radio" name="radio" checked />
+//         Radio - checked
+//       </label>
+//     </form>
+//   );
+// };
+
+// export default RadioBtn;
